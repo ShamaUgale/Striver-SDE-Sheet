fe64e473291912ce02bda5c3f7a0a5b697d0db8b
Shama Ugale <Shama.Ugale@gmail.com>
2022-08-31 21:16:50 +0530

    implemented Problem Statement:  Given a number check if it is a palindrome.

--- src/BasicMath/CheckNumberPalindrome.java
+++ src/BasicMath/CheckNumberPalindrome.java
@@ -1,13 +1,8 @@
 package BasicMath;
 
-/*
-
-Problem Statement: Given a number N reverse the number and print it.
-
- */
-public class ReverseNumber {
+public class CheckNumberPalindrome {
     public static void main(String[] args) {
-        int num = 423;
+        int num = 424;
         int temp = num;
         int rev = 0;
         while (temp != 0) {
@@ -15,6 +10,10 @@ public class ReverseNumber {
             rev = rev * 10 + remainder;
             temp = temp / 10;
         }
-        System.out.println(num + " number after reversing is : "+ rev);
+        if(num == rev){
+            System.out.println(num + " is a palindrome number");
+        }else{
+            System.out.println(num + " is not a palindrome number");
+        }
     }
 }
