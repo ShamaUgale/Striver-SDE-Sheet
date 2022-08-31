b2f8bd96228c524665d40aca075f36c9fd3c4c67
Shama Ugale <Shama.Ugale@gmail.com>
2022-08-31 21:05:51 +0530

    implemented Problem Statement: Given an integer N , write program to count number of digits in N.

---
+++ src/BasicMath/CountDigits.java
@@ -0,0 +1,20 @@
+package BasicMath;
+
+/*
+
+Problem Statement: Given an integer N , write program to count number of digits in N.
+
+
+ */
+public class CountDigits {
+    public static void main(String[] args) {
+        int num = 799999;
+        int count =0;
+        int temp = num;
+        while(temp != 0){
+            count++;
+            temp = temp/10;
+        }
+        System.out.println("Number of digits in num "+ num + " are : " + count);
+    }
+}
