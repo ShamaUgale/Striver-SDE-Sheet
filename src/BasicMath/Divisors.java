aa12c067381ff070e361767a42ad58890ef957e9
Shama Ugale <Shama.Ugale@gmail.com>
2022-08-31 21:44:46 +0530

    implemneted
    Sieve's algo
    
    Problem Statement: Given a number, print all the divisors of the number. A divisor is a number that gives remainder as zero when divided.

---
+++ src/BasicMath/Divisors.java
@@ -0,0 +1,19 @@
+package BasicMath;
+
+public class Divisors {
+    public static void main(String[] args) {
+        printDivisorsOptimal(6);
+    }
+
+    public static void printDivisorsOptimal(int n) {
+        System.out.println("The divisors of " + n + " are:");
+        for (int i = 1; i <= (int) Math.sqrt(n); i++)
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                if (i != n / i) {
+                    System.out.print(n / i + " ");
+                }
+            }
+        System.out.println();
+    }
+}
